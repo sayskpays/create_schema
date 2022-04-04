acf19b8bc117a794f7b665f5d00aa284934c6abb
sayskpays <sayskpayss@naver.com>
2022-04-04 17:35:39 +0900

    .

---
+++ demo/src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class MainController {
+    
+    @RequestMapping("/output")
+    public String requestMethodName(@RequestParam String param) {
+        return "output.html";
+    }
+    
+}
