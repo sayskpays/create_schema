1ec19a28600078633d397adfdfa7fdf1aff8f089
sayskpays <76785132+sayskpays@users.noreply.github.com>
2022-04-04 21:57:40 +0900

    .

--- demo/src/main/java/com/example/demo/controller/MainController.java
+++ demo/src/main/java/com/example/demo/controller/MainController.java
@@ -5,7 +5,7 @@ package com.example.demo.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -14,9 +14,13 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MainController {
     
-    @GetMapping("/hello")
+    @PostMapping("/hello")
     public ModelAndView hello(HttpServletRequest request) {
         ModelAndView mv = new ModelAndView();
+        String data = request.getParameter("id");
+        String data_type = request.getParameter("data_type");
+        System.out.println(data);
+        System.out.println(data_type);
         mv.setViewName("hello");
         return mv;
     }
